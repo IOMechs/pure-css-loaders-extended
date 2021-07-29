5cb408758659e2d7dda708d477733aac7c071d92
Dawood Shahid <dawoodshahid67@gmail.com>
2021-07-29 21:14:48 +0500

    Landing page header implemented without react routing

---
+++ src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,12 @@
+import './style.css'
+import Header from '../Header';
+
+const LandingPage = () => {
+  return (
+    <div className="landingPage">
+      <Header />
+    </div>
+  );
+};
+
+export default LandingPage;
