6e2dadf002a676ed55caec822feec9f6032208c8
Zuhair Naqi <zuhair.naqi@iomechs.com>
2021-07-31 23:58:17 +0500

    Fix: bind css style classes to loaders component

--- src/components/Loaders/Loaders.tsx
+++ src/components/Loaders/Loaders.tsx
@@ -1,19 +1,22 @@
-import { useEffect, useState, Children } from "react";
+import { useEffect, useState, Children, useRef } from "react";
 import LoadersData from '../../constants/getLoadersData';
 import Loader from "../../Interfaces/Loader";
 import './style.css';
 
 const LoadersComponent = () => {
   const [loaders, setLoaders] = useState<Loader[] | []>([]);
+  const loaderContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const styleEle = document.createElement("Style");
     styleEle.innerText = LoadersData.map(loaderData => loaderData.cssRules).join("");
-    document.head.appendChild(styleEle);
+    if (loaderContainerRef && loaderContainerRef.current) {
+      loaderContainerRef.current.append(styleEle);
+    }
     setLoaders([...LoadersData])
   }, [])
   return (
-    <div className="loaders-container">
+    <div ref={loaderContainerRef} className="loaders-container">
       {Children.toArray(
         loaders.map(loaderData => (
           <div dangerouslySetInnerHTML={{ __html: loaderData.html}} />
