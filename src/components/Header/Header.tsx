b35244043147557c32005b13ed232ba77fcf2552
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-08-01 02:08:29 +0200

    refactor(landing): refactor landing page for better UI
    added glassomorphic background
    fixed some layout issues
    fixed header styles
    removed commented code which are not necessary now
    changed the default color of the loaders to be white
    added a blurred background
    added credits
    implemented the github star button properly

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,31 @@
-import React from 'react';
-// import {} from 'react-router-dom';
 import './style.css';
 import logo from '../../assets/logo/iomechs-logo.png';
-import star from '../../assets/img/star.png';
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header_container">
-        <div className="logo_container">
+        <a
+          href="https://iomechs.com"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="logo_container"
+        >
           <img className="logo" src={logo} alt="IOMechs Logo" />
-        </div>
-        <div className="title header_text">
-          <p>Pure CSS Loaders</p>
-        </div>
+        </a>
+        <div className="title header_text">Pure CSS Loaders (configurable)</div>
         <div className="repo_link_container header_text">
-          <p>
-            Like Pure CSS Loaders?&nbsp;
-            <span> </span>
-            <a className="anchor" href="/">
-              Give our repo a star.
-              <span> </span>
-              <button className="btn">
-                <img className="star" src={star} alt="Star" />
-                <p>Star</p>
-              </button>
-              <span> </span>
-              <span>&#62;</span>
-            </a>
-          </p>
+          <span> Like Pure CSS Loaders? Give our repo a &nbsp; </span>
+          <a
+            className="github-button"
+            href="https://github.com/iomechs/pure-css-loaders"
+            data-icon="octicon-star"
+            aria-label="Star ntkme/github-buttons on GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Star
+          </a>
         </div>
       </div>
     </div>
